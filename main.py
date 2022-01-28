12586069e0d06eee15df4525d98e23a1ec6f30e0
Tanmay46dev <86778333+Tanmay46dev@users.noreply.github.com>
2022-01-28 15:27:43 +0530

    Update main.py
    Refactored the code

--- main.py
+++ main.py
@@ -1,36 +1,47 @@
-def log_in():
-	user_name = input("Type in your username: ")
-	password = input("Type in your password: ")
-	with open('accounts.txt') as f:
+def check_acc_existance(username, password):
+	with open("accounts.txt") as f:
 		for line in f.readlines():
-			line = line.replace(' ', '')
-			line = line.replace('\n', '')
-			if line.split('-')[0] == user_name:
-				if line.split('-')[1] == password:
-					print('Logged in successfuly!!')
-				else:
-					print(f"incorrect password for: {user_name}")
-			else:
-				print(f'No account named: {user_name}')
+			line = line.replace(" ", "").replace("\n", "")
+			acc_details = line.split("-")
+			if username == acc_details[0] and password == acc_details[1]:
+				return True
+	return False
 
+def get_user_credentials():
+	username = input("Username: ")
+	password = input("Password: ")
+	
+	return {"username": username, "password": password}
 
-def create_ac():
-	user_name = input("Choose your username: ")
-	password = input("Type in a strong password: ")
+def create_ac(username, password):
 	with open('accounts.txt', 'a') as f:
-		f.write(f"{user_name} - {password}\n")
+		f.write(f"{username} - {password}\n")
 	print("Account created successfuly!!")
 
 
 def main():
 	ans = input("Would you want to log in or create an account? (login/create): ")
 
+	credentials = get_user_credentials()
+	username = credentials.get("username")
+	password = credentials.get("password")
+
+	acc_already_exists = check_acc_existance(username, password)
+
 	if ans == 'login':
-		log_in()
+		if acc_already_exists:
+			print("Logged in successfully")
+		else:
+			print("Invalid credentials! Please try again.")
+			main()
 	elif ans == 'create':
-		create_ac()
+		if acc_already_exists:
+			print("Account already exists!")
+		else:
+			create_ac(username, password)
 	else:
 		print("Invalid option..")
+		main()
 
 if __name__ == '__main__':
 	main()
